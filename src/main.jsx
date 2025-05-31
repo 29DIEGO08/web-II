eaff3217494750bc8662417d2c6872553a4295ef
lgonzalezva <lgonzalezva123@gmail.com>
2025-05-31 15:22:38 -0500

    c1

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/routes";
 import { AuthProvider } from "./context/AuthContext";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <AppRoutes />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
