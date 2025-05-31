c71b5743433b907aa8d2196f19f4a946bfd48d2a
29DIEGO08 <programacion2905@email.com>
2025-05-31 08:26:28 -0500

    c10

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Routes from "./routes/routes";
+import AppRoutes from "./routes/routes";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Routes />
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes as RouterRoutes, Route } from "react-router-dom";
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
 import Participaciones from "../pages/páginasExclusivasUsuario/Participaciones";
@@ -8,22 +8,21 @@ import Formulario from "../pages/páginasExclusivasUsuario/Formulario";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
-function Routes() {
+function AppRoutes() {
   return (
     <Router>
-      <Routes>
+      <RouterRoutes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="participaciones" element={<Participaciones />} />
           <Route path="acerca" element={<AcercaDe />} />
           <Route path="formulario" element={<Formulario />} />
         </Route>
-      </Routes>
+      </RouterRoutes>
     </Router>
   );
 }
 
-export default Routes;
+export default AppRoutes;
