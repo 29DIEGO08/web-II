f0e5762a1ab09dfd2e4eea4e49fc2683f63ec744
29DIEGO08 <programacion2905@gmail.com>
2025-06-07 11:40:09 -0500

    c18

--- src/pages/páginasExclusivasAdmin/CrearEvento.jsx
+++ src/pages/páginasExclusivasAdmin/CrearEvento.jsx
@@ -21,7 +21,7 @@ export default function CrearEvento() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://eventos-25.onrender.com', {
+      const response = await fetch('https://eventos-25.onrender.com/api/eventos', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
